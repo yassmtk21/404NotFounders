432555327e2c6463cd72f5ab800bffed2c8ba067
yassmtk21 <yassinmoutik20@gmail.com>
2025-04-29 19:25:12 +0100

    new code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// vite.config.ts
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss()],
+});
